b34c29474238af4239619656d19ee2697857f911
camariana <camariana40@outlook.com>
2021-05-14 17:47:32 +0000

    Phonebook Exercises 3.13.-3.14

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
+
+const Person = require('./models/person')
+
+
+// Middlewares
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :contact'))
+app.use(cors())
+app.use(express.static('build'))
 
 
 const unknownEndpoint = (request, response) => {
@@ -19,40 +29,34 @@ morgan.token('contact', function (request, response)  {
 })
 
 
-// Middlewares
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :contact'))
-app.use(cors())
-app.use(express.static('build'))
 
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Ebrima Faal",
-        "number": "220-9998765"
-    },
-    {
-        "id": 2,
-        "name": "Cherno Faal",
-        "number": "220-8887654"
-    },
-    {
-        "id": 3,
-        "name": "Binta Dibba",
-        "number": "220-7776543"
-    },
-    {
-        "id": 4,
-        "name": "Lamin Barrow",
-        "number": "220-6665432"
-    },
-    {
-        "id": 5,
-        "name": "Ebrima Touray",
-        "number": "220-5554321"
-    }
-]
+// let persons = [
+//     {
+//         "id": 1,
+//         "name": "Ebrima Faal",
+//         "number": "220-9998765"
+//     },
+//     {
+//         "id": 2,
+//         "name": "Cherno Faal",
+//         "number": "220-8887654"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Binta Dibba",
+//         "number": "220-7776543"
+//     },
+//     {
+//         "id": 4,
+//         "name": "Lamin Barrow",
+//         "number": "220-6665432"
+//     },
+//     {
+//         "id": 5,
+//         "name": "Ebrima Touray",
+//         "number": "220-5554321"
+//     }
+// ]
 
 
 // helper function/s
@@ -68,87 +72,84 @@ app.get('/', (request, response) => {
 
 // Info
 app.get('/info', (request, response) => {
-    const noOfPersons = persons.length
-    const time = new Date()
+    // const noOfPersons = persons.length
+    // const time = new Date()
 
-    response.send(`
-        <p>
-            Phonebook has info for ${noOfPersons} people
-        </p>
-        <p>
-            ${time}
-        </p>
-    `)
+    // response.send(`
+    //     <p>
+    //         Phonebook has info for ${noOfPersons} people
+    //     </p>
+    //     <p>
+    //         ${time}
+    //     </p>
+    // `)
 })
 
 
 // All person
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 
 
 // Single person
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-       return response.json(person); 
-    } else {
-        return response.status(404).end() 
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person);
+    })
 })
 
 // delete person
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.filter(person => person.id !== id)
+    // const id = Number(request.params.id)
+    // const person = persons.filter(person => person.id !== id)
   
-    response.status(204).end()
+    // response.status(204).end()
 })
 
 // receiving data
 app.post('/api/persons', (request, response) => {
     const {name, number} = request.body
-    const isPerson = persons.some(person => person.name === name)
+   // const isPerson = persons.some(person => person.name === name)
    
-    if (isPerson) {
+    /* if (isPerson) {
         return response.status(400).json({ 
             error: `${name} already exists in the phonebook` 
         })
-    }
+    } */
 
-    if (!name ) {
+    if (name === undefined ) {
         return response.status(400).json({ 
             error: 'name missing' 
         })
     }
 
-    if (!number) {
+    if (number === undefined) {
         return response.status(400).json({ 
             error: 'number missing' 
         })
     }
 
 
-    const person = {
-        id: generateId(),
+    const person = new  Person({
         name,
         number,
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+        mongoose.connection.close()
+   })
 })
 
  
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
